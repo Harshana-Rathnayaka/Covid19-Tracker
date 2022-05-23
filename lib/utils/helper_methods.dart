5153a1c6ef513703d0ac4f1edc3da985af565326
Harshana Rathnayaka <huratnayaka@gmail.com>
2022-05-23 08:15:10 +0530

    error toast widget added

--- lib/utils/helper_methods.dart
+++ lib/utils/helper_methods.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 
 var numberFormat = NumberFormat('###,###.##');
+
+void showToast(String msg, [bool error = true]) {
+  Fluttertoast.showToast(msg: msg, textColor: Colors.white, backgroundColor: error ? Colors.red : Colors.green);
+}
