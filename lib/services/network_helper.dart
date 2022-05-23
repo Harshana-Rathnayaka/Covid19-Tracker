c35065e927b1d0a3b0c8d5517369c2e539ce3b60
Harshana Rathnayaka <huratnayaka@gmail.com>
2022-05-23 08:16:10 +0530

    exceptions and timeout added

--- lib/services/network_helper.dart
+++ lib/services/network_helper.dart
@@ -1,22 +1,17 @@
-import 'package:connectivity/connectivity.dart';
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 
 class Network {
   final String url = "https://disease.sh/v3/covid-19";
 
-  getData(endpoint) async {
+  Future getData(endpoint) async {
     var fullUrl = url + endpoint;
 
-    return await http.get(Uri.parse(fullUrl));
-  }
-
-  checkInternetConnection() async {
-    var connectivityResult = await Connectivity().checkConnectivity();
-
-    if (connectivityResult == ConnectivityResult.none) {
-      return false;
-    } else if (connectivityResult == ConnectivityResult.wifi || connectivityResult == ConnectivityResult.mobile) {
-      return true;
+    try {
+      return await http.get(Uri.parse(fullUrl)).timeout(Duration(seconds: 10));
+    } on SocketException {
+      throw SocketException('No internet connection.');
     }
   }
 }
